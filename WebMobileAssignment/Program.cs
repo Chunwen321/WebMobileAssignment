facee27d9893892900813bce07a31a291c5a4516
LeaveMeAlone0729 <chewzhexuan134@gmail.com>
2025-11-14 12:28:21 +0800

    Update Program.cs

--- WebMobileAssignment/Program.cs
+++ WebMobileAssignment/Program.cs
@@ -2,7 +2,11 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 app.MapGet("/", () => "Hello Chunwen!");
+=======
+app.MapGet("/", () => "Hello chunwen!");
+>>>>>>> Stashed changes
 =======
 app.MapGet("/", () => "Hello chunwen!");
 >>>>>>> Stashed changes
