bfb437e704460e074549eea905612e4c733abbae
Chunwen <gohchunwen@gmail.com>
2025-11-14 12:34:01 +0800

    update
    asdasdasd

--- WebMobileAssignment/Program.cs
+++ WebMobileAssignment/Program.cs
@@ -1,7 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
+
 var app = builder.Build();
+app.UseHttpsRedirection();
+app.UseStaticFiles();  
+app.MapDefaultControllerRoute();
+app.Run();
 
 app.MapGet("/", () => "Hello chunwen!");
-
 
 app.Run();
